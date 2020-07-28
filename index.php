fd2427779d1daf29d8826c1eaebf85d2cecf6648
Bisma Bendesa <bismabendesajaya@gmail.com>
2020-07-28 12:15:50 -0700

    menambahkan 10 list item

--- index.php
+++ index.php
@@ -8,5 +8,17 @@
 <body>
     <h1>Hello world!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta incidunt suscipit velit sed nam at commodi dolore enim officia. Dolor quod commodi at. Odio, placeat deleniti totam fugiat quod tempora.</p>
+    <ul>
+    <li>ichi</li>
+    <li>ni</li>
+    <li>san</li>
+    <li>shi</li>
+    <li>go</li>
+    <li>roku</li>
+    <li>nana</li>
+    <li>hachi</li>
+    <li>kyuu</li>
+    <li>jyuu</li>
+    </ul>
 </body>
 </html>
